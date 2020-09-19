c2b03c0169d632e8d06c283835d11e65e1172b7a
parisDreyer <lukepdreyer@gmail.com>
2020-09-19 07:43:12 -0400

    Initial Commit

---
+++ ToDoAssistant/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoAssistant
+//
+//  Created by Luke Dreyer on 9/19/20.
+//  Copyright © 2020 Luke Dreyer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
