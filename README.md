9166bf6d7cfc948f2f9f5cce97bfb54a461311d9
parisDreyer <lukepdreyer@gmail.com>
2022-04-24 07:28:31 -0400

    Update readme and fix a couple linter errors

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # ToDoAssistant
 iOS App to help keep track of stuff i'm working on
 
-http://todoassistant.com/
+Some upcoming goals for the project
+
+TODO (in order of priority):
+
+- set up backend that stores longterm text sequence associations (another git repo for this)
+- send frontend stored text associations to backend
+- frontend requests subset of backend stored text associations
+- if frontend does not have an immediate response to a text input, request a backend response for associated text input
+- dynamically categorize subsets of text association and have the frontend request text associations by category depending on frontend input context
+- schedule a job that processes backend stored text associations to distill "essential" text association groups and prune unneeded text entries
+
--- ToDoAssistant/Survey/ResearchKitSurvey/SurveyTaskFactory.swift
+++ ToDoAssistant/Survey/ResearchKitSurvey/SurveyTaskFactory.swift
@@ -19,52 +19,6 @@ extension UIColor {
 }
 
 final class SurveyTaskFactory {
-    public static func makeExampleSurvey() -> ORKOrderedTask {
-
-        var steps = [ORKStep]()
-
-        let instructionStep = ORKInstructionStep(identifier: "IntroStep")
-        instructionStep.title = "The Questions Three"
-        instructionStep.text = "Who would cross the Bridge of Death must answer me these questions three, ere the other side they see."
-        steps += [instructionStep]
-
-        let nameAnswerFormat = ORKTextAnswerFormat(maximumLength: 20)
-        nameAnswerFormat.multipleLines = false
-        let nameQuestionStepTitle = "What is your name?"
-        let nameQuestionStep = ORKQuestionStep(identifier: "QuestionStep", title: nameQuestionStepTitle, answer: nameAnswerFormat)
-        steps += [nameQuestionStep]
-
-        let questQuestionStepTitle = "What is your quest?"
-        let textChoices = [
-            ORKTextChoice(text: "Create a ResearchKit App", value: ORKFieldValue(value: 0)),
-            ORKTextChoice(text: "Seek the Holy Grail", value: ORKFieldValue(value: 1)),
-            ORKTextChoice(text: "Find a shrubbery", value: ORKFieldValue(value: 2))
-        ]
-        let questAnswerFormat: ORKTextChoiceAnswerFormat = ORKAnswerFormat.choiceAnswerFormat(with: .singleChoice, textChoices: textChoices)
-        let questQuestionStep = ORKQuestionStep(identifier: "TextChoiceQuestionStep", title: questQuestionStepTitle, answer: questAnswerFormat)
-        steps += [questQuestionStep]
-
-        let colorQuestionStepTitle = "What is your favorite color?"
-        let imageChoices : [ORKImageChoice] = [
-            (UIColor.red.image(), UIColor.red),
-            (UIColor.magenta.image(), UIColor.magenta),
-            (UIColor.yellow.image(), UIColor.yellow),
-            (UIColor.green.image(), UIColor.green),
-            (UIColor.blue.image(), UIColor.blue)
-        ].map { (image, color) -> ORKImageChoice in
-            return ORKImageChoice(normalImage: image, selectedImage: nil, text: String(describing: color), value: color)
-        }
-        let colorAnswerFormat: ORKImageChoiceAnswerFormat = ORKAnswerFormat.choiceAnswerFormat(with: imageChoices)
-        let colorQuestionStep = ORKQuestionStep(identifier: "ImageChoiceQuestionStep", title: colorQuestionStepTitle, answer: colorAnswerFormat)
-        steps += [colorQuestionStep]
-
-        let summaryStep = ORKCompletionStep(identifier: "SummaryStep")
-        summaryStep.title = "Right. Off you go!"
-        summaryStep.text = "That was easy!"
-        steps += [summaryStep]
-
-        return ORKOrderedTask(identifier: "SurveyTask", steps: steps)
-    }
 
     public static func makeBotSurvey(surveyQuestions: SurveyQuestions) -> ORKOrderedTask {
         let steps = makeSurveySteps(surveyQuestions)
@@ -94,7 +48,7 @@ private extension SurveyTaskFactory {
             return ORKTextChoice(text: text, value: ORKFieldValue(value: index))
         }
         let answer: ORKTextChoiceAnswerFormat = ORKAnswerFormat.choiceAnswerFormat(with: .singleChoice, textChoices: textChoices)
-        return ORKQuestionStep(identifier: question, title: question, answer: answer)
+        return ORKQuestionStep(identifier: question, title: nil, question: question, answer: answer)//(identifier: question, title: question, answer: answer)
     }
 
 }
